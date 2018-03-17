29bec921b3663c6de0e32fabac76f56dafadd715
Kyle Lin <kylejlin.us@gmail.com>
2018-03-16 22:31:22 -0700

    Export compileStringToString in index.js

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import { compileStringToString } from '../index'
+
+const code = `
+(pickRandom "foo" 9 (sum 1 2 3))
+`
+
+const js = compileStringToString(code)
+
+test('it generates the correct js', () => {
+  expect(js).toMatchSnapshot()
+})
